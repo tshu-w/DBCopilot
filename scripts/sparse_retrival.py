9912eb8f54ae3dd08e5067ba9d130f1e66031427
Tianshu Wang <wang@tianshu.me>
2023-08-02 07:49:19 +0000

    Add logics to get golden scores

--- scripts/sparse_retrival.py
+++ scripts/sparse_retrival.py
@@ -33,6 +33,9 @@ def generate_collection(schemas, resolution) -> dict[str, str]:
 
 
 def generate_qrels(instances, resolution) -> dict[str, dict]:
+    if resolution.startswith("all_"):
+        resolution = resolution[len("all_") :]
+
     qrels = {}
     for i, it in enumerate(instances):
         if resolution == "column":
@@ -55,34 +58,55 @@ def generate_qrels(instances, resolution) -> dict[str, dict]:
 
 def retrieve_schemas(
     dataset: Literal["spider", "bird"],
-    resolution: Literal["database", "table", "column"],
+    resolution: Literal["database", "table", "column", "all_table", "all_column"],
 ):
     retriever = SparseRetriever(index_name=f"data/{dataset}.index")
     with Path(f"data/{dataset}_schemas.json").open() as f:
         schemas = json.load(f)
 
-    retriever = retriever.index(
-        generate_collection(schemas.items(), resolution),
-        show_progress=False,
-    )
+    if "all" not in resolution:
+        retriever = retriever.index(
+            generate_collection(schemas.items(), resolution),
+            show_progress=False,
+        )
 
     for path in Path("data").glob(f"{dataset}_test*.json"):
         with path.open() as f:
             test = json.load(f)
 
         queries = [{"id": str(i), "text": it["question"]} for i, it in enumerate(test)]
-        results = retriever.bsearch(queries, show_progress=False)
+        if "all" not in resolution:
+            results = retriever.bsearch(queries, show_progress=False)
+        elif resolution == "all_table":
+            results = {
+                str(i): {
+                    f"{it['schema']['database']}.{tbl['name']}": 1
+                    for tbl in schemas[it["schema"]["database"]]
+                }
+                for i, it in enumerate(test)
+            }
+        elif resolution == "all_column":
+            results = {
+                str(i): {
+                    f"{it['schema']['database']}.{tbl['name']}.{col}": 1
+                    for tbl in schemas[it["schema"]["database"]]
+                    for col in tbl["columns"]
+                }
+                for i, it in enumerate(test)
+            }
+
         qrels = Qrels(generate_qrels(test, resolution))
         run = Run(results)
-        metrics = ["recall@1", "recall@5", "recall@10"]
+        metrics = ["recall@1", "recall@5", "recall@10", "f1@10", "f1"]
         print(
             path.stem,
             resolution,
             evaluate(qrels, run, metrics=metrics),
+            sep="\t",
         )
 
 
 if __name__ == "__main__":
     for dataset in ["spider", "bird"]:
-        for resolution in ["database", "table", "column"]:
+        for resolution in ["database", "table", "column", "all_table", "all_column"]:
             retrieve_schemas(dataset, resolution)
