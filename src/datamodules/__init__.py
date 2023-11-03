8e072c5b01850e08c53c03b96dd4d92a2afd6ce3
Tianshu Wang <wang@tianshu.me>
2023-11-03 07:07:17 +0000

    Remove the redundant datamodule

--- src/datamodules/__init__.py
+++ src/datamodules/__init__.py
@@ -1,5 +1,4 @@
 from .schema2text import Schema2Text
-from .schema_text import SchemaText
 from .text2schema import Text2Schema
 
-__all__ = ["Schema2Text", "Text2Schema", "SchemaText"]
+__all__ = ["Schema2Text", "Text2Schema"]
